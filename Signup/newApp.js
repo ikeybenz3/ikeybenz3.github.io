9b2e5872bd16bc0c8e7beb2a8805936f5c5cd732
Ikey Benzaken <ikey.benz@gmail.com>
2019-02-04 23:49:52 -0800

    Some front page updates and now allowing three signup options

--- Signup/newApp.js
+++ Signup/newApp.js
@@ -2,34 +2,37 @@ var stripeHandler = StripeCheckout.configure({
     key: 'pk_live_Vek1WDHXK9AjMQcZJxaQSVEY',
     image: '../LogoWithWhite.png',
     locale: 'auto',
-    token: function(tkn) {
-      let amnt = $('#paymentAmount').val();
-      $.ajax({
-        url: `https://jsc-payment-processor.herokuapp.com/charge`, 
-        data: {token: tkn.id, amount: amnt},
-        statusCode: {
-          200: () => {
-            alert('Payment Succeeded\nPlease sign the following waiver to complete your signup.');
-            window.location.replace('https://waiver.fr/p-4WyUT');
-          },
-          500: alert('Payment Failed\nSomething went wrong.')  
-        }
-      });
-    }  
+    token: function (tkn) {
+        let amnt = $('#paymentAmount').val();
+        $.ajax({
+            url: `https://jsc-payment-processor.herokuapp.com/charge`,
+            data: { token: tkn.id, amount: amnt },
+            statusCode: {
+                200: () => {
+                    alert('Payment Succeeded\nPlease sign the following waiver to complete your signup.');
+                    window.location.replace('https://waiver.fr/p-4WyUT');
+                },
+                500: alert('Payment Failed\nSomething went wrong.')
+            }
+        });
+    }
 });
-function showStripeForm() {
-    let amnt = $('#paymentAmount').val();
-    let desc = $('#bonusWeek').prop('checked') ? 'Full Summer + Bonus Week' : 'Full Summer';
+function showStripeForm(type) {
+    if (type != 'full') {
+        $('#paymentAmount').val('154380');
+    }
+    const amnt = $('#paymentAmount').val();
+    const desc = (type == 'full') ? $('#bonusWeek').prop('checked') ? 'Full Summer + Bonus Week' : 'Full Summer' : type;
     stripeHandler.open({
         name: 'Jersey Surf Camp LLC',
         description: desc,
         amount: amnt
     });
 }
-window.addEventListener('popstate', function() {
+window.addEventListener('popstate', function () {
     handler.close();
 });
-$(document).ready(function() {
+$(document).ready(function () {
     $('#bonusWeek').on('click', (e) => {
         if ($('#bonusWeek').prop('checked')) {
             $('#total').text('$3,398.87');
